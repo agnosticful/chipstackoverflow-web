7c6692c36248b090d43a8f8053c4d74047b3f5f7
Kohei Asai <yo@kohei.dev>
2020-03-02 19:41:10 -0800

    Setup with Next.js

---
+++ pages/index.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+import styled from "styled-components";
+
+export default function Home() {
+  return <Root>Hello!</Root>;
+}
+
+const Root = styled.span`
+  color: red;
+`;
