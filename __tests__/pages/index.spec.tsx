9461f5f400065e2a5f05c1c63dda1e8080286964
ryotogashi <ryo.togashi.ca@gmail.com>
2020-03-04 01:17:59 -0800

    changed by review.

--- __tests__/pages/index.spec.tsx
+++ __tests__/pages/index.spec.tsx
@@ -1,9 +1,9 @@
-import * as React from "react";
-import * as TestRenderer from "react-test-renderer";
-import Home from "../../pages/index";
+import * as React from 'react';
+import * as TestRenderer from 'react-test-renderer';
+import Home from '../../pages/index';
 
-describe("Home test", () => {
-  it("tests home page render correctly", () => {
+describe('Home test', () => {
+  it('tests home page render correctly', () => {
     const renderer = TestRenderer.create(<Home />);
 
     expect(renderer.toJSON()).toMatchSnapshot();
