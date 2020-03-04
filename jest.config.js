136f68c5497e91557469b4e957c19401fe0a4e1d
ryotogashi <ryo.togashi.ca@gmail.com>
2020-03-03 23:36:59 -0800

    fix conflict

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  preset: 'ts-jest',
+  globals: {
+    'ts-jest': {
+      tsConfig: {
+        ...require('./tsconfig.json').compilerOptions,
+        jsx: 'react'
+      }
+    }
+  }
+};
