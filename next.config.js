a32f37211800f6cb4230598d3eac15ae0a20a8e5
Kohei Asai <yo@kohei.dev>
2020-03-09 09:58:26 -0700

    Use authentication in index page

---
+++ next.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  env: {
+    firebaseAppOptions: JSON.stringify({
+      apiKey: "AIzaSyDYyfXxY0SDh-frzVbWjqWvDBDXHDVFzFI",
+      authDomain: "chipstackoverflow.firebaseapp.com",
+      databaseURL: "https://chipstackoverflow.firebaseio.com",
+      projectId: "chipstackoverflow",
+      storageBucket: "chipstackoverflow.appspot.com",
+      messagingSenderId: "373238358080",
+      appId: "1:373238358080:web:ab932ac823e7dca9e63fa5",
+      measurementId: "G-7081C73TLP"
+    })
+  }
+};
