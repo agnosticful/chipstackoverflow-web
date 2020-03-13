6baddd0f509460a35469b7f3631252a59f5f816c
Kohei Asai <yo@kohei.dev>
2020-03-13 08:26:29 -0700

    Add RankIcon, SuitIcon and PlayingCard components (#41)

---
+++ constants/color.ts
@@ -0,0 +1,7 @@
+export const CARD_BACKGROUND = "#f7fbff";
+export const CARD_DARK_BACKGROUND = "#1e272e";
+export const SPADE_COLOR = "#576574";
+export const INVERSE_SPADE_COLOR = "#F1F5F8";
+export const HEART_COLOR = "#ff6b6b";
+export const DIAMOND_COLOR = "#54a0ff";
+export const CLUB_COLOR = "#1dd1a1";
