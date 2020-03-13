583960ad64f6d4a2693fc20c1f95b15333bb79c0
ryotogashi <ryo.togashi.ca@gmail.com>
2020-03-13 01:28:09 -0700

    add createNewPost

---
+++ repositories/createNewPost.ts
@@ -0,0 +1,33 @@
+import * as firebase from 'firebase';
+import { PostTitle, PostBody, PostId } from '../models/Post';
+import { UserId } from '../models/User';
+import GameSituation from '../models/GameSituation';
+
+export interface PostData {
+  userId: UserId;
+  title: PostTitle;
+  body: PostBody;
+  gameSituation: GameSituation;
+}
+
+export type CreateNewPost = (postData: PostData) => Promise<PostId>;
+
+export function createCreateNewPost({ firebaseApp }: { firebaseApp: firebase.app.App }): CreateNewPost {
+  return async ({ userId, title, body, gameSituation }: PostData) => {
+    const now = Date.now();
+
+    const firestore = firebaseApp.firestore();
+    const ref = await firestore.collection('posts').add({
+      user: firestore.collection('users').doc((userId as unknown) as string),
+      title: title,
+      body: body,
+      likes: 0,
+      dislikes: 0,
+      gameSituation: gameSituation,
+      createdAt: now,
+      lastUpdatedAt: now
+    });
+
+    return (ref.id as unknown) as PostId;
+  };
+}
