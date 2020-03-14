4bb6b826c38989f5dfb47b36d834836d94e447c0
ryotogashi <ryo.togashi.ca@gmail.com>
2020-03-14 12:47:08 -0700

    delete return.

--- components/PostCardListItem.tsx
+++ components/PostCardListItem.tsx
@@ -40,59 +40,57 @@ const PostCardListItem = ({
   posted,
   isRecentPost,
   onClick
-}: Props) => {
-  return (
-    <Card
-      hoverable
-      style={{ maxWidth: "496px", margin: "8px" }}
-      bodyStyle={{ padding: "8px" }}
-      onClick={onClick}
-    >
-      <Row gutter={[4, 4]}>
-        <Col span={5}>
-          <PlayerHand>
-            <PlayingCard
-              suit={hand[0].suit}
-              rank={hand[0].rank}
-              style={{ width: "40%", margin: "2px" }}
-            />
-            <PlayingCard
-              suit={hand[1].suit}
-              rank={hand[1].rank}
-              style={{ width: "40%", margin: "2px" }}
-            />
-          </PlayerHand>
-        </Col>
-        <Col span={19}>
-          <Typography.Title level={4} style={{ margin: "0 0 8px 8px" }}>
-            {title}
-          </Typography.Title>
-        </Col>
-      </Row>
-      <Row justify="space-between">
-        <Col span={4}>
-          <CenterItem>
-            <LikeIcon
-              color="gray"
-              style={{ width: 25, height: 25, margin: "4px" }}
-            />
-            {likes}
-          </CenterItem>
-        </Col>
-        <Attribute title="PLAY AT" value={playAt} />
-        <Attribute title="ENDED AT" value={endedAt} />
-        <Attribute
-          title="FINAL POT"
-          value={`${getSIMetricPrefixData(finalPod)} BB`}
-        />
-        <Attribute
-          title={isRecentPost ? "POSTED" : "Last Update"}
-          value={getAgoByDate(posted)}
-        />
-      </Row>
-    </Card>
-  );
-};
+}: Props) => (
+  <Card
+    hoverable
+    style={{ maxWidth: "496px", margin: "8px" }}
+    bodyStyle={{ padding: "8px" }}
+    onClick={onClick}
+  >
+    <Row gutter={[4, 4]}>
+      <Col span={5}>
+        <PlayerHand>
+          <PlayingCard
+            suit={hand[0].suit}
+            rank={hand[0].rank}
+            style={{ width: "40%", margin: "2px" }}
+          />
+          <PlayingCard
+            suit={hand[1].suit}
+            rank={hand[1].rank}
+            style={{ width: "40%", margin: "2px" }}
+          />
+        </PlayerHand>
+      </Col>
+      <Col span={19}>
+        <Typography.Title level={4} style={{ margin: "0 0 8px 8px" }}>
+          {title}
+        </Typography.Title>
+      </Col>
+    </Row>
+    <Row justify="space-between">
+      <Col span={4}>
+        <CenterItem>
+          <LikeIcon
+            color="gray"
+            style={{ width: 25, height: 25, margin: "4px" }}
+          />
+          {likes}
+        </CenterItem>
+      </Col>
+      <Attribute title="PLAY AT" value={playAt} />
+      <Attribute title="ENDED AT" value={endedAt} />
+      <Attribute
+        title="FINAL POT"
+        value={`${getSIMetricPrefixData(finalPod)} BB`}
+      />
+      <Attribute
+        title={isRecentPost ? "POSTED" : "Last Update"}
+        value={getAgoByDate(posted)}
+      />
+    </Row>
+  </Card>
+);
 
 const Attribute = ({ title, value }: { title: string; value: string }) => (
   <Col>
