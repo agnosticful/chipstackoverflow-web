a42f7bc74983286742ff2ab0dbfd577d6591b22c
ryotogashi <ryo.togashi.ca@gmail.com>
2020-03-14 03:00:12 -0700

    add RecentPostCardList

---
+++ components/RecentPostCardList.stories.tsx
@@ -0,0 +1,147 @@
+import * as React from 'react';
+
+import RecentPostCardList from './RecentPostCardList';
+import { Suit, Rank } from '../models/PlayingCard';
+import Position from '../models/Position';
+import Round from '../models/Round';
+
+export default {
+  title: 'RecentPosts',
+  component: RecentPostCardList
+};
+
+export const recentPostItem = () => {
+  const postData: {
+    id: string;
+    hand: [{ rank: Rank; suit: Suit }, { rank: Rank; suit: Suit }];
+    title: string;
+    likes: number;
+    playAt: Position;
+    endedAt: Round;
+    finalPod: number;
+    posted: Date;
+  }[] = [
+    {
+      id: 'fawegewaugwegj23452fse',
+      hand: [
+        {
+          suit: Suit.heart,
+          rank: Rank.jack
+        },
+        {
+          suit: Suit.diamond,
+          rank: Rank.ace
+        }
+      ],
+      title: 'This is post title this is post title this is post title',
+      likes: 125,
+      playAt: Position.UTG,
+      endedAt: Round.PREFLOP,
+      finalPod: 2550.5,
+      posted: new Date(2020, 2, 10, 12, 15, 43, 100)
+    },
+    {
+      id: 'fawegewaugwegj2345agewagfse',
+      hand: [
+        {
+          suit: Suit.club,
+          rank: Rank.ace
+        },
+        {
+          suit: Suit.club,
+          rank: Rank.deuce
+        }
+      ],
+      title: 'This is post title this is post title this is post title',
+      likes: 125,
+      playAt: Position.UTG,
+      endedAt: Round.PREFLOP,
+      finalPod: 255000000.5,
+      posted: new Date(2020, 2, 14, 0, 15, 43, 100)
+    },
+    {
+      id: 'faweeeeeugwegj23452fse',
+      hand: [
+        {
+          suit: Suit.diamond,
+          rank: Rank.ace
+        },
+        {
+          suit: Suit.spade,
+          rank: Rank.king
+        }
+      ],
+      title: 'This is post title this is post title this is post title',
+      likes: 125,
+      playAt: Position.UTG,
+      endedAt: Round.PREFLOP,
+      finalPod: 255000000000.5,
+      posted: new Date(2020, 1, 4, 12, 15, 43, 100)
+    },
+    {
+      id: 'fawegewaugwg423452fse',
+      hand: [
+        {
+          suit: Suit.club,
+          rank: Rank.ace
+        },
+        {
+          suit: Suit.diamond,
+          rank: Rank.ace
+        }
+      ],
+      title: 'This is post title this is post title this is post title',
+      likes: 125,
+      playAt: Position.UTG,
+      endedAt: Round.PREFLOP,
+      finalPod: 255000000000000.5,
+      posted: new Date(2019, 3, 4, 12, 15, 43, 100)
+    },
+    {
+      id: 'h55hs5h5s4hg',
+      hand: [
+        {
+          suit: Suit.club,
+          rank: Rank.ace
+        },
+        {
+          suit: Suit.diamond,
+          rank: Rank.ace
+        }
+      ],
+      title: 'This is post title this is post title this is post title',
+      likes: 125,
+      playAt: Position.UTG,
+      endedAt: Round.PREFLOP,
+      finalPod: 255000000000000000.5,
+      posted: new Date(2020, 2, 4, 12, 15, 43, 100)
+    },
+    {
+      id: 'j7j7jrdruths',
+      hand: [
+        {
+          suit: Suit.club,
+          rank: Rank.ace
+        },
+        {
+          suit: Suit.diamond,
+          rank: Rank.ace
+        }
+      ],
+      title: 'This is post title this is post title this is post title',
+      likes: 125,
+      playAt: Position.UTG,
+      endedAt: Round.PREFLOP,
+      finalPod: 255.5,
+      posted: new Date(2020, 2, 4, 12, 15, 43, 100)
+    }
+  ];
+  return (
+    <RecentPostCardList
+      recentPostData={postData}
+      handleClick={id => {
+        console.log(`${id} is clicked`);
+      }}
+    />
+  );
+};
---
+++ components/RecentPostCardList.tsx
@@ -0,0 +1,63 @@
+import * as React from 'react';
+import styled from 'styled-components';
+import { Rank, Suit } from '../models/PlayingCard';
+import Position from '../models/Position';
+import Round from '../models/Round';
+import PostCardListItem from './PostCardListItem';
+
+interface Props {
+  recentPostData: {
+    id: string;
+    hand: [
+      {
+        rank: Rank;
+        suit: Suit;
+      },
+      {
+        rank: Rank;
+        suit: Suit;
+      }
+    ];
+    title: string;
+    likes: number;
+    playAt: Position;
+    endedAt: Round;
+    finalPod: number;
+    posted: Date;
+  }[];
+  handleClick: (id: string) => void;
+}
+
+const RecentPostCardList = ({ recentPostData, handleClick }: Props) => {
+  return (
+    <PostCardListDiv>
+      {recentPostData.map(({ id, hand, title, likes, playAt, endedAt, finalPod, posted }) => (
+        <PostCardListItem
+          key={id}
+          hand={hand}
+          title={title}
+          likes={likes}
+          playAt={playAt}
+          endedAt={endedAt}
+          finalPod={finalPod}
+          posted={posted}
+          onClick={() => {
+            handleClick(id);
+          }}
+        />
+      ))}
+    </PostCardListDiv>
+  );
+};
+
+const PostCardListDiv = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, 500px);
+  gap: 16px;
+
+  @media (max-width: 800px) {
+    grid-template-columns: repeat(1, 1fr);
+  }
+`;
+
+export default RecentPostCardList;
