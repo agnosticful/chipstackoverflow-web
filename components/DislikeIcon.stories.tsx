13c698cf989b6c9b98a4d51979ae07d563957c29
ryotogashi <ryo.togashi.ca@gmail.com>
2020-03-14 02:59:33 -0700

    add like and dislike icon

---
+++ components/DislikeIcon.stories.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+import DislikeIcon from './DislikeIcon';
+
+export default {
+  title: 'dislike',
+  component: DislikeIcon
+};
+
+export const icon = () => <DislikeIcon color='gray' style={{ width: 25, height: 25 }} />;
