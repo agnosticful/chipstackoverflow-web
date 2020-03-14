6dcc76fef5f8b3feb27077ee19e8d6b2ece2d9fb
ryotogashi <ryo.togashi.ca@gmail.com>
2020-03-14 16:47:10 -0700

    format

--- utilities/getFinalPodOfTheGame.ts
+++ utilities/getFinalPodOfTheGame.ts
@@ -1,10 +1,21 @@
-import GameSituation from '../models/GameSituation';
+import GameSituation from "../models/GameSituation";
 
-export default function getFinalPodOfTheGame(gameSituation: GameSituation): number {
-  const preflopPod = gameSituation.preflop.actions.reduce((sum, { betSize }) => sum + betSize, 0);
-  const flopPod = gameSituation.flop ? gameSituation.flop.actions.reduce((sum, { betSize }) => sum + betSize, 0) : 0;
-  const turnPod = gameSituation.turn ? gameSituation.turn.actions.reduce((sum, { betSize }) => sum + betSize, 0) : 0;
-  const riverPod = gameSituation.river ? gameSituation.river.actions.reduce((sum, { betSize }) => sum + betSize, 0) : 0;
+export default function getFinalPodOfTheGame(
+  gameSituation: GameSituation
+): number {
+  const preflopPod = gameSituation.preflop.actions.reduce(
+    (sum, { betSize }) => sum + betSize,
+    0
+  );
+  const flopPod = gameSituation.flop
+    ? gameSituation.flop.actions.reduce((sum, { betSize }) => sum + betSize, 0)
+    : 0;
+  const turnPod = gameSituation.turn
+    ? gameSituation.turn.actions.reduce((sum, { betSize }) => sum + betSize, 0)
+    : 0;
+  const riverPod = gameSituation.river
+    ? gameSituation.river.actions.reduce((sum, { betSize }) => sum + betSize, 0)
+    : 0;
 
   return preflopPod + flopPod + turnPod + riverPod;
 }
--- utilities/getPositionByPlayerAndIndex.ts
+++ utilities/getPositionByPlayerAndIndex.ts
@@ -1,17 +1,55 @@
-import Position from '../models/Position';
+import Position from "../models/Position";
 
-export default function getPositionByPlayerAndIndex(players: number, index: number): Position {
-  if (players < 2 || 10 < players) throw new Error('Player number must be 2 - 10.');
-  if (index < 0 || players < index) throw new Error(`Index number must be 0 - player number(${players})`);
+export default function getPositionByPlayerAndIndex(
+  players: number,
+  index: number
+): Position {
+  if (players < 2 || 10 < players)
+    throw new Error("Player number must be 2 - 10.");
+  if (index < 0 || players < index)
+    throw new Error(`Index number must be 0 - player number(${players})`);
 
   const playerNumberAndPoositionCorrespondenceMap = new Map([
     [2, [Position.SB, Position.BB]],
     [3, [Position.SB, Position.BB, Position.UTG]],
     [4, [Position.SB, Position.BB, Position.MP1, Position.BTN]],
     [5, [Position.SB, Position.BB, Position.MP1, Position.MP2, Position.BTN]],
-    [6, [Position.SB, Position.BB, Position.MP1, Position.MP2, Position.LP1, Position.BTN]],
-    [7, [Position.SB, Position.BB, Position.UTG, Position.MP1, Position.MP2, Position.LP1, Position.BTN]],
-    [8, [Position.SB, Position.BB, Position.UTG, Position.MP1, Position.MP2, Position.MP3, Position.LP1, Position.BTN]],
+    [
+      6,
+      [
+        Position.SB,
+        Position.BB,
+        Position.MP1,
+        Position.MP2,
+        Position.LP1,
+        Position.BTN
+      ]
+    ],
+    [
+      7,
+      [
+        Position.SB,
+        Position.BB,
+        Position.UTG,
+        Position.MP1,
+        Position.MP2,
+        Position.LP1,
+        Position.BTN
+      ]
+    ],
+    [
+      8,
+      [
+        Position.SB,
+        Position.BB,
+        Position.UTG,
+        Position.MP1,
+        Position.MP2,
+        Position.MP3,
+        Position.LP1,
+        Position.BTN
+      ]
+    ],
     [
       9,
       [
