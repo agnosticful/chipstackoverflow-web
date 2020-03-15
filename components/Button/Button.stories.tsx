a9c27e0738157f9eaf991c44ce57faeea213170f
Kohei Asai <yo@kohei.dev>
2020-03-15 10:59:40 -0700

    <Button> (#59)
    * Implement <Button>
    
    * Use made <Button> component instead of antd’s

---
+++ components/Button/Button.stories.tsx
@@ -0,0 +1,94 @@
+import * as React from "react";
+import { SignOutIcon } from "../Icon";
+import Button, { ButtonSize, ButtonVariant } from "./Button";
+import { select } from "@storybook/addon-knobs";
+
+export default {
+  title: "Button",
+  component: Button
+};
+
+export const example = () => (
+  <Button
+    variant={select("variant", VARIANTS, undefined)}
+    size={select("size", SIZES, undefined)}
+  >
+    Click me!
+  </Button>
+);
+
+export const variants = () => (
+  <>
+    <Button
+      variant={ButtonVariant.default}
+      size={select("size", SIZES, undefined)}
+    >
+      Default Button
+    </Button>
+    <Button
+      variant={ButtonVariant.primary}
+      size={select("size", SIZES, undefined)}
+    >
+      Primary Button
+    </Button>
+    <Button
+      variant={ButtonVariant.secondary}
+      size={select("size", SIZES, undefined)}
+    >
+      Secondary Button
+    </Button>
+  </>
+);
+
+export const sizes = () => (
+  <>
+    <Button
+      variant={select("variant", VARIANTS, undefined)}
+      size={ButtonSize.small}
+    >
+      Small Button
+    </Button>
+    <Button
+      variant={select("variant", VARIANTS, undefined)}
+      size={ButtonSize.regular}
+    >
+      Regular Button
+    </Button>
+    <Button
+      variant={select("variant", VARIANTS, undefined)}
+      size={ButtonSize.large}
+    >
+      Large Button
+    </Button>
+  </>
+);
+
+export const withIcon = () => (
+  <Button
+    variant={select("variant", VARIANTS, undefined)}
+    size={select("size", SIZES, undefined)}
+  >
+    <SignOutIcon /> Sign out
+  </Button>
+);
+
+export const Anchor = () => (
+  <Button
+    variant={select("variant", VARIANTS, undefined)}
+    size={select("size", SIZES, undefined)}
+  >
+    <a href="#">Go to next page</a>
+  </Button>
+);
+
+const VARIANTS = {
+  "ButtonVariant.default": ButtonVariant.default,
+  "ButtonVariant.primary": ButtonVariant.primary,
+  "ButtonVariant.secondary": ButtonVariant.secondary
+};
+
+const SIZES = {
+  "ButtonSize.large": ButtonSize.large,
+  "ButtonSize.regular": ButtonSize.regular,
+  "ButtonSize.small": ButtonSize.small
+};
---
+++ components/Button/Button.tsx
@@ -0,0 +1,155 @@
+import * as React from "react";
+import styled, { FlattenSimpleInterpolation, css } from "styled-components";
+
+interface Props extends React.Attributes {
+  variant?: ButtonVariant;
+  size?: ButtonSize;
+  onClick?: React.MouseEventHandler<HTMLButtonElement>;
+  className?: string;
+  style?: React.CSSProperties;
+  children?: React.ReactNode;
+}
+
+/**
+ * Write the description for this component here.
+ */
+export default function Button({
+  variant = ButtonVariant.default,
+  size = ButtonSize.regular,
+  children,
+  ...props
+}: Props) {
+  return (
+    <Root variant={variant} size={size} {...props}>
+      {children}
+    </Root>
+  );
+}
+
+export enum ButtonSize {
+  large,
+  regular,
+  small
+}
+
+export enum ButtonVariant {
+  default,
+  primary,
+  secondary
+}
+
+const Root = styled.button<{
+  variant: ButtonVariant;
+  size: ButtonSize;
+}>`
+  ${({ variant }) => VARIANT_CSS[variant]}
+  ${({ size }) => SIZE_CSS[size]}
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 4px;
+  outline: none;
+  -webkit-font-smoothing: antialiased;
+  cursor: pointer;
+  transition: background 200ms ease, border 200ms ease, color 200ms ease,
+    box-shadow 200ms ease, transform 200ms ease;
+
+  :hover {
+    transform: translate3d(0px, -2px, 0px);
+  }
+
+  & a {
+    color: inherit;
+    text-decoration: none;
+  }
+`;
+
+const VARIANT_CSS: Record<ButtonVariant, FlattenSimpleInterpolation> = {
+  [ButtonVariant.default]: css`
+    background: #0f151c;
+    border: 1px transparent solid;
+    color: #fff;
+
+    :hover {
+      background: #fff;
+      border: 1px #0f151c solid;
+      color: #0f151c;
+    }
+  `,
+  [ButtonVariant.primary]: css`
+    background: #0773fa;
+    border: 1px transparent solid;
+    color: #fff;
+
+    :hover {
+      background: #fff;
+      border: 1px #0773fa solid;
+      color: #0773fa;
+    }
+  `,
+  [ButtonVariant.secondary]: css`
+    background: #fff;
+    border: 1px #c8d6e5 solid;
+    color: #576574;
+
+    :hover {
+      border: 1px #0f151c solid;
+      color: #0f151c;
+    }
+  `
+};
+
+const SIZE_CSS: Record<ButtonSize, FlattenSimpleInterpolation> = {
+  [ButtonSize.large]: css`
+    min-width: 224px;
+    height: 48px;
+    padding: 0 24px;
+    font-size: 16px;
+    box-shadow: 0px 0px 12px #222f3e1f, 0px 12px 24px #222f3e0f;
+
+    :hover {
+      box-shadow: 0px 0px 12px #222f3e3f, 0px 12px 24px #222f3e1f;
+    }
+
+    & > svg {
+      width: 20px;
+      height: 20px;
+      margin-right: 8px;
+    }
+  `,
+  [ButtonSize.regular]: css`
+    min-width: 160px;
+    height: 40px;
+    padding: 0 16px;
+    font-size: 16px;
+    box-shadow: 0px 0px 10px #222f3e1f, 0px 10px 20px #222f3e0f;
+
+    :hover {
+      box-shadow: 0px 0px 10px #222f3e3f, 0px 10px 20px #222f3e1f;
+    }
+
+    & > svg {
+      width: 16px;
+      height: 16px;
+      margin-right: 8px;
+    }
+  `,
+  [ButtonSize.small]: css`
+    min-width: 96px;
+    height: 32px;
+    padding: 0 12px;
+    font-size: 14px;
+    box-shadow: 0px 0px 8px #222f3e1f, 0px 8px 16px #222f3e0f;
+
+    :hover {
+      box-shadow: 0px 0px 8px #222f3e3f, 0px 8px 16px #222f3e1f;
+    }
+
+    & > svg {
+      width: 16px;
+      height: 16px;
+      margin-right: 4px;
+    }
+  `
+};
