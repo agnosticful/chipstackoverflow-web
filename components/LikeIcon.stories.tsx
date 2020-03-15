ab677cad8a0d5803b8eb28d0bc1b0198537b699d
ryotogashi <ryo.togashi.ca@gmail.com>
2020-03-14 23:27:25 -0700

    add like icon.

---
+++ components/LikeIcon.stories.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+import LikeIcon from './LikeIcon';
+
+export default {
+  title: 'like',
+  component: LikeIcon
+};
+
+export const icon = () => <LikeIcon color='gray' style={{ width: 25, height: 25 }} />;
