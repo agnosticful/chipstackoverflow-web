9fee329a604f70951295a0b744f8be9756bc8332
ryotogashi <ryo.togashi.ca@gmail.com>
2020-03-15 04:02:11 -0700

    add LoadingPostCardListItem

---
+++ components/LoadingPostCardListItem.stories.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+import LoadingPostCardListItem from './LoadingPostCardListItem';
+
+export default {
+  title: 'LoadingPostCardListItem',
+  component: LoadingPostCardListItem
+};
+
+export const loadingPostCardListItem = () => <LoadingPostCardListItem />;
