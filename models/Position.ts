b43c96fbf6668646e19ef5adefe721d73128dbb4
ryotogashi <ryo.togashi.ca@gmail.com>
2020-03-14 23:59:42 -0700

    add getPositionByPlayerAndIndex and format

--- models/Position.ts
+++ models/Position.ts
@@ -1,14 +1,14 @@
 enum Position {
-  SB = 'SB',
-  BB = 'BB',
-  UTG = 'UTG',
-  EP2 = 'EP2',
-  MP1 = 'MP1',
-  MP2 = 'MP2',
-  MP3 = 'MP3',
-  LP1 = 'LP1',
-  LP2 = 'LP2',
-  BTN = 'BTN'
+  SB = "SB",
+  BB = "BB",
+  UTG = "UTG",
+  EP2 = "EP2",
+  MP1 = "MP1",
+  MP2 = "MP2",
+  MP3 = "MP3",
+  LP1 = "LP1",
+  LP2 = "LP2",
+  BTN = "BTN"
 }
 
 export default Position;
