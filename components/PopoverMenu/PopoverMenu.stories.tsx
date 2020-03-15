4a6d75d54c8cd370d74b32ebdaf72f703698d564
Kohei Asai <yo@kohei.dev>
2020-03-15 10:58:39 -0700

    Implement <PopoverMenu> and <PopoverMenuItem>

---
+++ components/PopoverMenu/PopoverMenu.stories.tsx
@@ -0,0 +1,69 @@
+import { action } from "@storybook/addon-actions";
+import * as React from "react";
+import { SignOutIcon } from "../Icon";
+import PopoverMenu from "./PopoverMenu";
+import PopoverMenuItem from "./PopoverMenuItem";
+
+export default {
+  title: "PopoverMenu",
+  component: PopoverMenu,
+  subcomponents: { PopoverMenuItem }
+};
+
+export const example = () => (
+  <div style={{ height: 200 }}>
+    <PopoverMenu
+      content={
+        <>
+          <PopoverMenuItem onClick={action('onClick ("Lorem Ipsum")')}>
+            Lorem Ipsum
+          </PopoverMenuItem>
+          <PopoverMenuItem onClick={action('onClick ("Settings")')}>
+            Settings
+          </PopoverMenuItem>
+          <PopoverMenuItem
+            icon={<SignOutIcon />}
+            onClick={action('onClick ("Sign out")')}
+          >
+            Sign out
+          </PopoverMenuItem>
+        </>
+      }
+    >
+      <button>Click me to show</button>
+    </PopoverMenu>
+  </div>
+);
+
+export const scrollPlayground = () => (
+  <div
+    style={{
+      display: "flex",
+      width: 2560,
+      height: 2560,
+      alignItems: "center",
+      justifyContent: "center"
+    }}
+  >
+    <PopoverMenu
+      content={
+        <>
+          <PopoverMenuItem onClick={action('onClick ("Lorem Ipsum")')}>
+            Lorem Ipsum
+          </PopoverMenuItem>
+          <PopoverMenuItem onClick={action('onClick ("Settings")')}>
+            Settings
+          </PopoverMenuItem>
+          <PopoverMenuItem
+            icon={<SignOutIcon />}
+            onClick={action('onClick ("Sign out")')}
+          >
+            Sign out
+          </PopoverMenuItem>
+        </>
+      }
+    >
+      <button>Click me to show</button>
+    </PopoverMenu>
+  </div>
+);
