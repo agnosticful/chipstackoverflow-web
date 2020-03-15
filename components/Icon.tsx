95a86ff81495c073c507ec57baf62d495c3a69c9
Kohei Asai <yo@kohei.dev>
2020-03-15 11:16:28 -0700

    Update <SignOutIcon> (#64)

--- components/Icon.tsx
+++ components/Icon.tsx
@@ -9,12 +9,12 @@ export function SignOutIcon(props: Props) {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      width="20"
-      height="20"
+      width="24"
+      height="24"
       viewBox="0 0 24 24"
       fill="none"
       stroke="currentColor"
-      strokeWidth="2"
+      strokeWidth="1.5"
       strokeLinecap="round"
       strokeLinejoin="round"
       {...props}
