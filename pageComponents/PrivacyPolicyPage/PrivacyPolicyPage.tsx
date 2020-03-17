00e3f5874851a8039372feaeb4d20a9ded7e4f71
Kohei Asai <yo@kohei.dev>
2020-03-16 21:34:55 -0700

    Delete an unnecessary section

--- pageComponents/PrivacyPolicyPage/PrivacyPolicyPage.tsx
+++ pageComponents/PrivacyPolicyPage/PrivacyPolicyPage.tsx
@@ -149,24 +149,6 @@ export default function PrivacyPolicyPage(props: Props) {
           information.
         </p>
 
-        <h2>Third Party Privacy Policies</h2>
-
-        <p>
-          <em>{WEBSITE_NAME}</em>'s Privacy Policy does not apply to other
-          advertisers or websites. Thus, we are advising you to consult the
-          respective Privacy Policies of these third-party ad servers for more
-          detailed information. It may include their practices and instructions
-          about how to opt-out of certain options. You may find a complete list
-          of these Privacy Policies and their links here: Privacy Policy Links.
-        </p>
-
-        <p>
-          You can choose to disable cookies through your individual browser
-          options. To know more detailed information about cookie management
-          with specific web browsers, it can be found at the browsers'
-          respective websites. What Are Cookies?
-        </p>
-
         <h2>CCPA Privacy Rights (Do Not Sell My Personal Information)</h2>
 
         <p>
