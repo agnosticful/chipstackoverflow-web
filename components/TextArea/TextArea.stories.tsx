9a4164864b0e2445dc69db917e88a4b9618363ad
ryotogashi <ryo.togashi.ca@gmail.com>
2020-03-17 14:25:43 -0700

    add TextArea

---
+++ components/TextArea/TextArea.stories.tsx
@@ -0,0 +1,32 @@
+import { action } from '@storybook/addon-actions';
+import { select } from '@storybook/addon-knobs';
+import * as React from 'react';
+import TextArea, { InputSize } from './TextArea';
+
+export default {
+  title: 'TextArea',
+  component: TextArea
+};
+
+export const example = () => <TextArea size={select('size', SIZES, undefined)} onChange={action('inputed')} />;
+
+export const Sizes = () => (
+  <>
+    <TextArea rows={5} size={InputSize.small} style={{ display: 'block' }} placeholder='Small' />
+    <TextArea rows={5} size={InputSize.medium} style={{ display: 'block' }} placeholder='Medium' />
+    <TextArea rows={5} size={InputSize.large} style={{ display: 'block' }} placeholder='Large' />
+  </>
+);
+export const fullWidth = () => (
+  <>
+    <TextArea fullWidth rows={5} size={InputSize.small} style={{ display: 'block' }} placeholder='Small' />
+    <TextArea fullWidth rows={5} size={InputSize.medium} style={{ display: 'block' }} placeholder='Medium' />
+    <TextArea fullWidth rows={5} size={InputSize.large} style={{ display: 'block' }} placeholder='Large' />
+  </>
+);
+
+const SIZES = {
+  'InputSize.large': InputSize.large,
+  'InputSize.medium': InputSize.medium,
+  'InputSize.small': InputSize.small
+};
