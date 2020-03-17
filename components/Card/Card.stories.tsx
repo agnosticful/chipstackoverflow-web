abc904520e8fe92d9d2cd9c69ebbcf21c1a34671
ryotogashi <ryo.togashi.ca@gmail.com>
2020-03-17 00:37:31 -0700

    delete  unnecessary space

--- components/Card/Card.stories.tsx
+++ components/Card/Card.stories.tsx
@@ -14,7 +14,7 @@ export const WithContent = () => (
     <h3>nibh tortor id</h3>
     <p>
       Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-      tempor incididunt ut labore et dolore magna aliqua.{" "}
+      tempor incididunt ut labore et dolore magna aliqua.
     </p>
   </Card>
 );
