4a5fa5cf439cee00e0ee0eac6747537a8f6d909d
ryotogashi <ryo.togashi.ca@gmail.com>
2020-03-17 13:11:46 -0700

    add grid to card

--- components/Card/Card.stories.tsx
+++ components/Card/Card.stories.tsx
@@ -1,6 +1,8 @@
 import { action } from "@storybook/addon-actions";
 import * as React from "react";
 import Card from "./Card";
+import styled from "styled-components";
+import { MOBILE_MEDIA } from "../../constants/mediaquery";
 
 export default {
   title: "Card",
@@ -18,3 +20,61 @@ export const WithContent = () => (
     </p>
   </Card>
 );
+export const InGrid = () => (
+  <Layout>
+    <Card onClick={action("clicked")}>
+      <h3>nibh tortor id</h3>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua.
+      </p>
+    </Card>
+    <Card onClick={action("clicked")}>
+      <h3>nibh tortor id</h3>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua.
+      </p>
+    </Card>
+    <Card onClick={action("clicked")}>
+      <h3>nibh tortor id</h3>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua.
+      </p>
+    </Card>
+    <Card onClick={action("clicked")}>
+      <h3>nibh tortor id</h3>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua.
+      </p>
+    </Card>
+    <Card onClick={action("clicked")}>
+      <h3>nibh tortor id</h3>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua.
+      </p>
+    </Card>
+    <Card onClick={action("clicked")}>
+      <h3>nibh tortor id</h3>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua.
+      </p>
+    </Card>
+  </Layout>
+);
+
+const Layout = styled.div`
+  display: grid;
+  grid-template-columns: 400px 400px;
+  grid-template-rows: 1fr 1fr 1fr;
+  grid-gap: 16px;
+
+  ${MOBILE_MEDIA} {
+    grid-template-columns: 100%;
+    grid-template-rows: 1fr 1fr 1fr 1fr 1fr 1fr;
+  }
+`;
