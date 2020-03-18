0c03ad929b621615306dd6b0dce359819401d042
ryotogashi <ryo.togashi.ca@gmail.com>
2020-03-18 02:51:41 -0700

    use firebase function

--- repositories/createNewPost.ts
+++ repositories/createNewPost.ts
@@ -1,10 +1,8 @@
 import * as firebase from "firebase";
 import GameSituation from "../models/GameSituation";
 import { PostTitle, PostBody, PostId } from "../models/Post";
-import { UserId } from "../models/User";
 
 export interface PostData {
-  userId: UserId;
   title: PostTitle;
   body: PostBody;
   gameSituation: GameSituation;
@@ -17,21 +15,14 @@ export function createCreateNewPost({
 }: {
   firebaseApp: firebase.app.App;
 }): CreateNewPost {
-  return async ({ userId, title, body, gameSituation }: PostData) => {
-    const now = Date.now();
-
-    const firestore = firebaseApp.firestore();
-    const ref = await firestore.collection("posts").add({
-      user: firestore.collection("users").doc((userId as unknown) as string),
+  return async ({ title, body, gameSituation }: PostData) => {
+    const createPost = firebaseApp.functions().httpsCallable("createPost");
+    const id = await createPost({
       title: title,
       body: body,
-      likes: 0,
-      dislikes: 0,
-      gameSituation: gameSituation,
-      createdAt: now,
-      lastUpdatedAt: now
+      gameSituation: gameSituation
     });
 
-    return (ref.id as unknown) as PostId;
+    return (id as unknown) as PostId;
   };
 }
