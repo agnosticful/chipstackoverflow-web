5d8a7ae8d94fcdc511f8ba6281c15b4a97d31f87
Ryo Togashi <35892492+ryotogashi@users.noreply.github.com>
2020-03-18 20:25:15 -0700

    <Card> (#75)
    * add card
    
    * add story and format
    
    * fix by review.
    
    * add action to card
    
    * changed by review and add display: inline-block.
    
    * add min width and height
    
    * delete onclick
    
    * delete  unnecessary space
    
    * add grid to card
    
    * transition
    
    * delete default height and width
    
    * add non-clickable story
    
    * add line and order import
    
    * changed by review
    
    * change import order

---
+++ components/Card/Card.stories.tsx
@@ -0,0 +1,98 @@
+import { action } from "@storybook/addon-actions";
+import * as React from "react";
+import styled from "styled-components";
+import { MOBILE_MEDIA } from "../../constants/mediaquery";
+import Card from "./Card";
+
+export default {
+  title: "Card",
+  component: Card
+};
+
+export const example = () => (
+  <Card style={{ height: "200px", width: "400px" }}>400 x 200</Card>
+);
+
+export const clickable = () => (
+  <Card onClick={action("onClick")} style={{ height: "200px", width: "400px" }}>
+    400 x 200
+  </Card>
+);
+
+export const withContent = () => (
+  <CardWithContent onClick={action("onClick")}>
+    <h3>nibh tortor id</h3>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua.
+    </p>
+  </CardWithContent>
+);
+
+export const inGrid = () => (
+  <Layout>
+    <Card onClick={action("onClick")}>
+      <h3>nibh tortor id</h3>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua.
+      </p>
+    </Card>
+    <Card onClick={action("onClick")}>
+      <h3>nibh tortor id</h3>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua.
+      </p>
+    </Card>
+    <Card onClick={action("onClick")}>
+      <h3>nibh tortor id</h3>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua.
+      </p>
+    </Card>
+    <Card onClick={action("onClick")}>
+      <h3>nibh tortor id</h3>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua.
+      </p>
+    </Card>
+    <Card onClick={action("onClick")}>
+      <h3>nibh tortor id</h3>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua.
+      </p>
+    </Card>
+    <Card onClick={action("onClick")}>
+      <h3>nibh tortor id</h3>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua.
+      </p>
+    </Card>
+  </Layout>
+);
+
+const CardWithContent = styled(Card)`
+  padding: 16px;
+`;
+
+const Layout = styled.div`
+  display: grid;
+  grid-template-columns: 400px 400px;
+  grid-template-rows: 1fr 1fr 1fr;
+  grid-gap: 16px;
+
+  & h3,
+  & p {
+    margin: 16px;
+  }
+
+  ${MOBILE_MEDIA} {
+    grid-template-columns: 100%;
+    grid-template-rows: 1fr 1fr 1fr 1fr 1fr 1fr;
+  }
+`;
