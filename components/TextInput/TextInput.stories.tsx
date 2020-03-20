7d377bd7468d1cd2efecade305a55ed4d791b070
Kohei Asai <yo@kohei.dev>
2020-03-19 18:16:01 -0700

    <TextInput> (#104)
    * Implement <TextInput> component
    
    * Refactor code
    
    * Format code

---
+++ components/TextInput/TextInput.stories.tsx
@@ -0,0 +1,32 @@
+import { action } from "@storybook/addon-actions";
+import { select, text } from "@storybook/addon-knobs";
+import * as React from "react";
+import TextInput, { TextInputSize } from "./TextInput";
+
+export default {
+  title: "TextInput",
+  component: TextInput
+};
+
+export const example = () => (
+  <TextInput
+    size={select("size", SIZES, TextInputSize.medium)}
+    defaultValue={text("defaultValue", "")}
+    placeholder={text("placeholder", "Lorem ipsum ...")}
+    onChange={action("onChange")}
+  />
+);
+
+export const large = () => (
+  <TextInput
+    size={TextInputSize.large}
+    defaultValue={text("defaultValue", "")}
+    placeholder={text("placeholder", "Lorem ipsum ...")}
+    onChange={action("onChange")}
+  />
+);
+
+const SIZES = {
+  "TextInputSize.medium": TextInputSize.medium,
+  "TextInputSize.large": TextInputSize.large
+};
