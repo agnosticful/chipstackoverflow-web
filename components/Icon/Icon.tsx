beff0d2fb9af819a79b5f32793670294bbbb5468
Kohei Asai <yo@kohei.dev>
2020-03-22 18:30:46 -0700

    Add <LoadingIcon> component

--- components/Icon/Icon.tsx
+++ components/Icon/Icon.tsx
@@ -14,6 +14,25 @@ export function ChevronDownIcon(props: Props) {
   );
 }
 
+export function LoadingIcon(props: Props) {
+  return (
+    <LoadingIconRoot
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      {...props}
+    >
+      <line x1="12" y1="2" x2="12" y2="6"></line>
+      <line x1="12" y1="18" x2="12" y2="22"></line>
+      <line x1="4.93" y1="4.93" x2="7.76" y2="7.76"></line>
+      <line x1="16.24" y1="16.24" x2="19.07" y2="19.07"></line>
+      <line x1="2" y1="12" x2="6" y2="12"></line>
+      <line x1="18" y1="12" x2="22" y2="12"></line>
+      <line x1="4.93" y1="19.07" x2="7.76" y2="16.24"></line>
+      <line x1="16.24" y1="7.76" x2="19.07" y2="4.93"></line>
+    </LoadingIconRoot>
+  );
+}
+
 export function PlusIcon(props: Props) {
   return (
     <Root xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" {...props}>
@@ -67,4 +86,17 @@ const Root = styled.svg`
   stroke-linecap: round;
   stroke-linejoin: round;
   transition: width 200ms ease, height 200ms ease;
+`;
+
+const LoadingIconRoot = styled(Root)`
+  @keyframes spin {
+    0% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(359deg);
+    }
+  }
+
+  animation: spin 1000ms linear infinite;
 `;
