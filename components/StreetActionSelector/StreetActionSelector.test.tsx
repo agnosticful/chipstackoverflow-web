a239b4bf2bbb0b73ba0b8dd6c5622c68ca895c92
Kohei Asai <yo@kohei.dev>
2020-03-26 20:11:20 -0700

    Add tests for <StreetActionSelector>

--- components/StreetActionSelector/StreetActionSelector.test.tsx
+++ components/StreetActionSelector/StreetActionSelector.test.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import { act, create } from "react-test-renderer";
 import { createRenderer } from "react-test-renderer/shallow";
 import StreetActionSelector from "./StreetActionSelector";
 
@@ -35,28 +36,243 @@ describe("<StreetActionSelector>", () => {
     expect(renderer.getRenderOutput()).toMatchSnapshot();
   });
 
-  // TODO: (https://github.com/agnosticful/chipstackoverflow-web/issues/49)
-  it.todo(
-    "throws when props.previousBetSize that more than props.tableMaxBetSize given"
-  );
+  it("throws when props.previousBetSize is more than props.tableMaxBetSize", async () => {
+    const onChange = jest.fn().mockName("onChange");
+    const renderer = createRenderer();
 
-  it.todo(
-    'calls onChange() with props.previousBetSize when "Fold" button is clicked'
-  );
+    expect(() =>
+      renderer.render(
+        <StreetActionSelector
+          tableMaxBetSize={1}
+          previousBetSize={2}
+          minBetSizeDiff={1}
+          onChange={onChange}
+        />
+      )
+    ).toThrow();
+  });
 
-  it.todo(
-    'calls onChange() with props.tableMaxBetSize when "Call" button is clicked'
-  );
+  it('calls onChange() with props.previousBetSize when "Fold" button is clicked', async () => {
+    const onChange = jest.fn().mockName("onChange");
+    const renderer = create(
+      <StreetActionSelector
+        tableMaxBetSize={1}
+        previousBetSize={0}
+        minBetSizeDiff={1}
+        onChange={onChange}
+      />
+    );
 
-  it.todo(
-    'calls onChange() with the sum of props.tableMaxBetSize and props.minBetSizeDiff when "Raise" button is clicked'
-  );
+    const foldButton = renderer.root.find(
+      node => node.props["data-test-id"] === "fold-button"
+    );
 
-  it.todo(
-    "calls onChange() with the value of input element whenever its value changes"
-  );
+    await act(async () => {
+      foldButton.props.onClick();
+    });
 
-  it.todo(
-    "doesn't call onChange() even when input element's value changes as long as \"Raise\" is not selected"
-  );
+    await act(async () => {
+      renderer.update(
+        <StreetActionSelector
+          tableMaxBetSize={3}
+          previousBetSize={1}
+          minBetSizeDiff={2}
+          onChange={onChange}
+        />
+      );
+    });
+
+    await act(async () => {
+      foldButton.props.onClick();
+    });
+
+    expect(onChange).toHaveBeenCalledTimes(2);
+    expect(onChange).toHaveBeenNthCalledWith(1, 0);
+    expect(onChange).toHaveBeenNthCalledWith(2, 1);
+  });
+
+  it('calls onChange() with props.tableMaxBetSize when "Call" button is clicked', async () => {
+    const onChange = jest.fn().mockName("onChange");
+    const renderer = create(
+      <StreetActionSelector
+        tableMaxBetSize={1}
+        previousBetSize={0}
+        minBetSizeDiff={1}
+        onChange={onChange}
+      />
+    );
+
+    const checkOrCallButton = renderer.root.find(
+      node => node.props["data-test-id"] === "check-or-call-button"
+    );
+
+    await act(async () => {
+      checkOrCallButton.props.onClick();
+    });
+
+    await act(async () => {
+      renderer.update(
+        <StreetActionSelector
+          tableMaxBetSize={3}
+          previousBetSize={1}
+          minBetSizeDiff={2}
+          onChange={onChange}
+        />
+      );
+    });
+
+    await act(async () => {
+      checkOrCallButton.props.onClick();
+    });
+
+    expect(onChange).toHaveBeenCalledTimes(2);
+    expect(onChange).toHaveBeenNthCalledWith(1, 1);
+    expect(onChange).toHaveBeenNthCalledWith(2, 3);
+  });
+
+  it("sets defaultValue at bet size input that is sum of props.tableMaxBetSize and props.minBetSizeDiff", async () => {
+    const renderer = create(
+      <StreetActionSelector
+        tableMaxBetSize={1}
+        previousBetSize={0}
+        minBetSizeDiff={1}
+      />
+    );
+
+    let betSizeInput = renderer.root.find(
+      node => node.props["data-test-id"] === "bet-size-input"
+    );
+
+    expect(betSizeInput.props.defaultValue).toBe(2);
+
+    await act(async () => {
+      renderer.update(
+        <StreetActionSelector
+          tableMaxBetSize={3}
+          previousBetSize={1}
+          minBetSizeDiff={2}
+        />
+      );
+    });
+
+    betSizeInput = renderer.root.find(
+      node => node.props["data-test-id"] === "bet-size-input"
+    );
+
+    expect(betSizeInput.props.defaultValue).toBe(5);
+  });
+
+  it('calls onChange() with the bet size input\'s value when "Raise" button is clicked', async () => {
+    const onChange = jest.fn().mockName("onChange");
+    const betSizeInputValue = 123456789;
+    const renderer = create(
+      <StreetActionSelector
+        tableMaxBetSize={1}
+        previousBetSize={0}
+        minBetSizeDiff={1}
+        onChange={onChange}
+      />,
+      {
+        createNodeMock: element => {
+          if (element.props["data-test-id"] === "bet-size-input") {
+            return {
+              focus: () => undefined,
+              value: betSizeInputValue
+            };
+          }
+
+          return null;
+        }
+      }
+    );
+
+    const betOrRaiseButton = renderer.root.find(
+      node => node.props["data-test-id"] === "bet-or-raise-button"
+    );
+
+    await act(async () => {
+      betOrRaiseButton.props.onClick();
+    });
+
+    expect(onChange).toHaveBeenCalledTimes(1);
+    expect(onChange).toHaveBeenNthCalledWith(1, betSizeInputValue);
+  });
+
+  it('sets a focus on bet size input when "Raise" button is clicked', async () => {
+    const focus = jest.fn().mockName("focus");
+    const renderer = create(
+      <StreetActionSelector
+        tableMaxBetSize={1}
+        previousBetSize={0}
+        minBetSizeDiff={1}
+      />,
+      {
+        createNodeMock: element => {
+          if (element.props["data-test-id"] === "bet-size-input") {
+            return {
+              focus,
+              value: null
+            };
+          }
+
+          return null;
+        }
+      }
+    );
+
+    const betOrRaiseButton = renderer.root.find(
+      node => node.props["data-test-id"] === "bet-or-raise-button"
+    );
+
+    await act(async () => {
+      betOrRaiseButton.props.onClick();
+    });
+
+    expect(focus).toHaveBeenCalledTimes(1);
+  });
+
+  it("calls onChange() with the value of input element whenever its value changes", async () => {
+    const onChange = jest.fn().mockName("onChange");
+    const renderer = create(
+      <StreetActionSelector
+        tableMaxBetSize={1}
+        previousBetSize={0}
+        minBetSizeDiff={1}
+        onChange={onChange}
+      />
+    );
+
+    let betSizeInput = renderer!.root.find(
+      node => node.props["data-test-id"] === "bet-size-input"
+    );
+
+    await act(async () => {
+      betSizeInput.props.onChange({ currentTarget: { value: 20 } });
+    });
+
+    await act(async () => {
+      renderer.update(
+        <StreetActionSelector
+          tableMaxBetSize={3}
+          previousBetSize={1}
+          minBetSizeDiff={2}
+          onChange={onChange}
+        />
+      );
+    });
+
+    // re-obtain betSizeInput element
+    // because it would be remounted whenever props.tableMaxBetSize changes
+    betSizeInput = renderer!.root.find(
+      node => node.props["data-test-id"] === "bet-size-input"
+    );
+
+    await act(async () => {
+      betSizeInput.props.onChange({ currentTarget: { value: 43 } });
+    });
+
+    expect(onChange).toHaveBeenCalledTimes(2);
+    expect(onChange).toHaveBeenNthCalledWith(1, 20);
+    expect(onChange).toHaveBeenNthCalledWith(2, 43);
+  });
 });
