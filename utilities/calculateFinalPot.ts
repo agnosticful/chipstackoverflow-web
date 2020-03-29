cdf59e085f93625f4b94261e9ca22e329a9e1c6f
ryotogashi <ryo.togashi.ca@gmail.com>
2020-03-29 11:00:47 -0700

    change valuable name by review.

--- utilities/calculateFinalPot.ts
+++ utilities/calculateFinalPot.ts
@@ -3,34 +3,38 @@ import GameSituation from "../models/GameSituation";
 export default function calculateFinalPot(
   gameSituation: GameSituation
 ): number {
-  const preflopPot: number[] = new Array(gameSituation.playerLength).fill(0);
-  for (const { playerIndex, betSize } of gameSituation.preflop.actions) {
-    preflopPot[playerIndex] = betSize;
-  }
+  const lastPlayerBetSizesOnPreflop: number[] = new Array(
+    gameSituation.playerLength
+  ).fill(0);
+  for (const { playerIndex, betSize } of gameSituation.preflop.actions)
+    lastPlayerBetSizesOnPreflop[playerIndex] = betSize;
 
-  const flopPot: number[] = new Array(gameSituation.playerLength).fill(0);
+  const lastPlayerBetSizesOnFlopPot: number[] = new Array(
+    gameSituation.playerLength
+  ).fill(0);
   if (gameSituation.flop)
-    for (const { playerIndex, betSize } of gameSituation.flop.actions) {
-      flopPot[playerIndex] = betSize;
-    }
+    for (const { playerIndex, betSize } of gameSituation.flop.actions)
+      lastPlayerBetSizesOnFlopPot[playerIndex] = betSize;
 
-  const turnPot: number[] = new Array(gameSituation.playerLength).fill(0);
+  const lastPlayerBetSizesOnTurnPot: number[] = new Array(
+    gameSituation.playerLength
+  ).fill(0);
   if (gameSituation.turn)
-    for (const { playerIndex, betSize } of gameSituation.turn.actions) {
-      turnPot[playerIndex] = betSize;
-    }
+    for (const { playerIndex, betSize } of gameSituation.turn.actions)
+      lastPlayerBetSizesOnTurnPot[playerIndex] = betSize;
 
-  const riverPot: number[] = new Array(gameSituation.playerLength).fill(0);
+  const lastPlayerBetSizesOnRiverPot: number[] = new Array(
+    gameSituation.playerLength
+  ).fill(0);
   if (gameSituation.river)
-    for (const { playerIndex, betSize } of gameSituation.river.actions) {
-      riverPot[playerIndex] = betSize;
-    }
+    for (const { playerIndex, betSize } of gameSituation.river.actions)
+      lastPlayerBetSizesOnRiverPot[playerIndex] = betSize;
 
   return (
     gameSituation.antiSize * gameSituation.playerLength +
-    preflopPot.reduce((pre, curr) => pre + curr, 0) +
-    flopPot.reduce((pre, curr) => pre + curr, 0) +
-    turnPot.reduce((pre, curr) => pre + curr, 0) +
-    riverPot.reduce((pre, curr) => pre + curr, 0)
+    lastPlayerBetSizesOnPreflop.reduce((pre, curr) => pre + curr, 0) +
+    lastPlayerBetSizesOnFlopPot.reduce((pre, curr) => pre + curr, 0) +
+    lastPlayerBetSizesOnTurnPot.reduce((pre, curr) => pre + curr, 0) +
+    lastPlayerBetSizesOnRiverPot.reduce((pre, curr) => pre + curr, 0)
   );
 }
