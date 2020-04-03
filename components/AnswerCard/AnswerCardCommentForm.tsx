ad2f4df6befd0ddb73dae71ac2bba50611ce0aba
Kohei Asai <yo@kohei.dev>
2020-04-02 19:39:09 -0700

    Implement <AnswerCard> and its descendant components

---
+++ components/AnswerCard/AnswerCardCommentForm.tsx
@@ -0,0 +1,122 @@
+import * as React from "react";
+import styled from "styled-components";
+import User from "../../models/User";
+import Avatar from "../Avatar";
+import Button from "../Button";
+import { LoadingIcon, SendIcon } from "../Icon";
+import TextArea from "../TextArea";
+
+export interface Props extends React.Attributes {
+  user: User;
+  defaultValue?: string;
+  submitting?: boolean;
+  invalid?: boolean;
+  invalidReason?: string;
+  onChange?: (e: React.ChangeEvent<HTMLTextAreaElement>, body: string) => void;
+  onSubmit?: (e: React.SyntheticEvent<HTMLElement>, body: string) => void;
+  className?: string;
+  style?: React.CSSProperties;
+}
+
+/**
+ * A part of `<AnswerCard>` that represents to create a new comment.
+ */
+export default function AnswerCardCommentForm({
+  user,
+  defaultValue = "",
+  submitting,
+  invalid,
+  invalidReason,
+  onChange = () => undefined,
+  onSubmit = () => undefined,
+  ...props
+}: Props) {
+  const [textAreaId, setTextAreaId] = React.useState(Math.random());
+  const [body, setBody] = React.useState(defaultValue);
+  const onBodyChange = React.useCallback(
+    (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+      setBody(e.currentTarget.value);
+      onChange(e, e.currentTarget.value);
+    },
+    [onChange]
+  );
+  const onSubmitClick = React.useCallback(
+    (e: React.MouseEvent<HTMLElement>) => {
+      setBody("");
+      setTextAreaId(Math.random());
+      onSubmit(e, body);
+    },
+    [onSubmit, body]
+  );
+
+  return (
+    <Root {...props}>
+      <Author>
+        <AuthorAvatar src={user.profileImageURL.href} />
+
+        <AuthorName>{user.name}</AuthorName>
+      </Author>
+
+      <_TextArea
+        placeholder="Leave your comment ..."
+        defaultValue={defaultValue}
+        disabled={submitting || invalid}
+        onChange={onBodyChange}
+        key={textAreaId}
+      />
+
+      {invalidReason ? <HelpfulMessage>{invalidReason}</HelpfulMessage> : null}
+
+      <SubmitButton disabled={submitting || invalid} onClick={onSubmitClick}>
+        {submitting ? <LoadingIcon /> : <SendIcon />}
+
+        {submitting ? "Submitting..." : "Submit"}
+      </SubmitButton>
+    </Root>
+  );
+}
+
+const Root = styled.div`
+  display: grid;
+  grid-template-columns: 64px 1fr;
+  grid-template-areas: "author author author" ". textarea textarea" ". helpful-message submit-button";
+`;
+
+const Author = styled.div`
+  grid-area: author;
+  display: grid;
+  grid-template-columns: auto auto;
+  grid-template-areas: "avatar name";
+  justify-content: flex-start;
+  align-items: center;
+  column-gap: 16px;
+`;
+
+const AuthorAvatar = styled(Avatar)`
+  grid-area: avatar;
+  width: 48px;
+  height: 48px;
+`;
+
+const AuthorName = styled.div`
+  grid-area: name;
+  font-size: 16px;
+  font-weight: 700;
+`;
+
+const _TextArea = styled(TextArea)`
+  grid-area: textarea;
+`;
+
+const HelpfulMessage = styled.div`
+  grid-area: helpful-message;
+  margin-top: 16px;
+  color: #576574;
+  font-size: 12px;
+`;
+
+const SubmitButton = styled(Button)`
+  grid-area: submit-button;
+  justify-self: flex-end;
+  margin-top: 16px;
+`;
---
+++ components/AnswerCard/AnswerCardContentLoader.tsx
@@ -0,0 +1,170 @@
+import * as React from "react";
+import styled from "styled-components";
+import ContentLoader from "react-content-loader";
+
+export interface Props extends React.Attributes {
+  className?: string;
+  style?: React.CSSProperties;
+  children?: React.ReactNode;
+}
+
+/**
+ * A part of `<AnswerCard>` that shows loading placeholder as answers or comments
+ */
+export default function AnswerCardContentLoader({ children, ...props }: Props) {
+  return (
+    <Root {...props}>
+      <UserAvatar />
+
+      <UserName />
+
+      <CreationDate />
+
+      <BodyFirstLine />
+      <BodySecondLine />
+      <BodyThirdLine />
+      <BodyFourthLine />
+      <BodyFifthLine />
+
+      <LikeIcon />
+      <LikeNumber />
+
+      <DislikeIcon />
+      <DislikeNumber />
+    </Root>
+  );
+}
+
+const Root = styled(ContentLoader)`
+  --avatar-diameter: 48px;
+  --margin-right-of-avatar: 16px;
+  --body-height: 16px;
+  --body-line-height: calc(16px * 1.333);
+  --body-vertical-padding: calc(
+    (var(--body-line-height) - var(--body-height)) / 2
+  );
+  --reaction-icon-size: 24px;
+  width: 100%;
+  height: calc(
+    var(--avatar-diameter) + var(--body-line-height) * 5 + 16px +
+      var(--reaction-icon-size)
+  );
+`;
+
+const UserAvatar = styled.circle`
+  cx: calc(var(--avatar-diameter) / 2);
+  cy: calc(var(--avatar-diameter) / 2);
+  r: calc(var(--avatar-diameter) / 2);
+`;
+
+const UserName = styled.rect`
+  --height: 16px;
+  x: calc(var(--avatar-diameter) + var(--margin-right-of-avatar));
+  y: calc((var(--avatar-diameter) - var(--height)) / 2);
+  rx: 4px;
+  ry: 4px;
+  width: 128px;
+  height: var(--height);
+`;
+
+const CreationDate = styled.rect`
+  --width: 64px;
+  --height: 14px;
+  x: calc(100% - var(--width));
+  y: calc((var(--avatar-diameter) - var(--height)) / 2);
+  rx: 3px;
+  ry: 3px;
+  width: var(--width);
+  height: var(--height);
+`;
+
+const BodyFirstLine = styled.rect`
+  x: calc(var(--avatar-diameter) + var(--margin-right-of-avatar));
+  y: calc(var(--avatar-diameter));
+  rx: 3px;
+  ry: 3px;
+  width: 95%;
+  height: var(--body-height);
+`;
+
+const BodySecondLine = styled.rect`
+  x: calc(var(--avatar-diameter) + var(--margin-right-of-avatar));
+  y: calc(var(--avatar-diameter) + var(--body-line-height));
+  rx: 3px;
+  ry: 3px;
+  width: 85%;
+  height: var(--body-height);
+`;
+
+const BodyThirdLine = styled.rect`
+  x: calc(var(--avatar-diameter) + var(--margin-right-of-avatar));
+  y: calc(var(--avatar-diameter) + var(--body-line-height) * 2);
+  rx: 3px;
+  ry: 3px;
+  width: 75%;
+  height: var(--body-height);
+`;
+
+const BodyFourthLine = styled.rect`
+  x: calc(var(--avatar-diameter) + var(--margin-right-of-avatar));
+  y: calc(var(--avatar-diameter) + var(--body-line-height) * 3);
+  rx: 3px;
+  ry: 3px;
+  width: 90%;
+  height: var(--body-height);
+`;
+
+const BodyFifthLine = styled.rect`
+  x: calc(var(--avatar-diameter) + var(--margin-right-of-avatar));
+  y: calc(var(--avatar-diameter) + var(--body-line-height) * 4);
+  rx: 3px;
+  ry: 3px;
+  width: 60%;
+  height: var(--body-height);
+`;
+
+const LikeIcon = styled.rect`
+  x: calc(var(--avatar-diameter) + var(--margin-right-of-avatar));
+  y: calc(var(--avatar-diameter) + var(--body-line-height) * 5 + 16px);
+  rx: 3px;
+  ry: 3px;
+  width: var(--reaction-icon-size);
+  height: var(--reaction-icon-size);
+`;
+
+const LikeNumber = styled.rect`
+  x: calc(
+    var(--avatar-diameter) + var(--margin-right-of-avatar) +
+      var(--reaction-icon-size) + 8px
+  );
+  y: calc(var(--avatar-diameter) + var(--body-line-height) * 5 + 16px + 4px);
+  rx: 3px;
+  ry: 3px;
+  width: 24px;
+  height: 16px;
+`;
+
+const DislikeIcon = styled.rect`
+  x: calc(
+    var(--avatar-diameter) + var(--margin-right-of-avatar) +
+      var(--reaction-icon-size) + 8px + 24px + 16px
+  );
+  y: calc(var(--avatar-diameter) + var(--body-line-height) * 5 + 16px);
+  rx: 3px;
+  ry: 3px;
+  width: var(--reaction-icon-size);
+  height: var(--reaction-icon-size);
+`;
+
+const DislikeNumber = styled.rect`
+  x: calc(
+    var(--avatar-diameter) + var(--margin-right-of-avatar) +
+      var(--reaction-icon-size) + 8px + 24px + 16px + var(--reaction-icon-size) +
+      8px
+  );
+  y: calc(var(--avatar-diameter) + var(--body-line-height) * 5 + 16px + 4px);
+  rx: 3px;
+  ry: 3px;
+  width: 24px;
+  height: 16px;
+`;
