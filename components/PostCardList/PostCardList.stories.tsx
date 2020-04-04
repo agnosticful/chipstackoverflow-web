56b5fdd3f6e29a30a0e8febffb87adca5b7d6c6b
Ryo Togashi <35892492+ryotogashi@users.noreply.github.com>
2020-04-04 15:01:02 -0700

    <PostCardList> (#137)
    * Implement getStringWithSIMetricSuffix (#135)
    
    * add getStringWithSIMetricSuffix
    
    * change map name by review.
    
    * Implement getPositionByPlayerAndIndex (#133)
    
    * add getPositionByPlayerAndIndex
    
    * changed by review.
    
    * Implement getRelativeDateString (#134)
    
    * add getRelativeDateString
    
    * changed by review.
    
    * Implement calculateFinalPot (#132)
    
    * calculateFinalPot
    
    * fix wrong test data
    
    * changed by review.
    
    * fix by review and add change cases.
    
    * changed by review.
    
    * change valuable name by review.
    
    * add curly braces
    
    * PostCardList View changed by review (#131)
    
    * changed by reivew at #105
    
    * use utils
    
    * changed by review.
    
    * add calclateFinalPot
    
    * changed by review.
    
    * Delete getRelativeDateString utility function and use date-fns instead (#155)
    
    * use date-fns
    
    * delete getRelativeDateString

---
+++ components/PostCardList/PostCardList.stories.tsx
@@ -0,0 +1,110 @@
+import { action } from "@storybook/addon-actions";
+import { boolean } from "@storybook/addon-knobs";
+import * as React from "react";
+import GameSituation, { GameType } from "../../models/GameSituation";
+import { Suit, Rank } from "../../models/PlayingCard";
+import Post, { PostId, PostTitle, PostBody } from "../../models/Post";
+import { UserId } from "../../models/User";
+import PostCardList from "./PostCardList";
+import PostCardListItem from "./PostCardListItem";
+
+export default {
+  title: "PostCardList",
+  component: PostCardList,
+  subcomponent: { PostCardListItem }
+};
+
+export const example = () => {
+  return (
+    <PostCardList showLastUpdateDate={boolean("showLastUpdate", false)}>
+      <PostCardListItem post={post} onClick={action("onClick")} />
+      <PostCardListItem post={post} onClick={action("onClick")} />
+      <PostCardListItem post={post} onClick={action("onClick")} />
+      <PostCardListItem post={post} onClick={action("onClick")} />
+      <PostCardListItem post={post} onClick={action("onClick")} />
+      <PostCardListItem post={post} onClick={action("onClick")} />
+    </PostCardList>
+  );
+};
+
+export const showLastUpdateDate = () => (
+  <>
+    <PostCardList showLastUpdateDate>
+      <PostCardListItem post={post} onClick={action("onClick")} />
+      <PostCardListItem post={post} onClick={action("onClick")} />
+      <PostCardListItem post={post} onClick={action("onClick")} />
+      <PostCardListItem post={post} onClick={action("onClick")} />
+      <PostCardListItem post={post} onClick={action("onClick")} />
+      <PostCardListItem post={post} onClick={action("onClick")} />
+    </PostCardList>
+  </>
+);
+
+const post: Post = {
+  id: "rgaergba" as PostId,
+  user: "gagaegaerga" as UserId,
+  title: "suspendisse interdum consectetur libero id undisse interdum consectetur libero deid consectetur libero deid" as PostTitle,
+  body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." as PostBody,
+  likes: 125,
+  dislikes: 12,
+  gameSituation: ({
+    type: GameType.cash,
+    playerLength: 2,
+    playerStackSizes: [0, 0],
+    heroIndex: 0,
+    playerCards: [
+      {
+        left: {
+          rank: Rank.seven,
+          suit: Suit.heart
+        },
+        right: {
+          rank: Rank.eight,
+          suit: Suit.diamond
+        }
+      }
+    ],
+    smallBlindSize: 0.05,
+    antiSize: 0,
+    preflop: {
+      actions: [
+        {
+          playerIndex: 0,
+          betSize: 0.05
+        },
+        {
+          playerIndex: 1,
+          betSize: 0.05
+        }
+      ]
+    },
+    flop: {
+      communityCards: {
+        left: {
+          suit: Suit.diamond,
+          rank: Rank.five
+        },
+        center: {
+          suit: Suit.diamond,
+          rank: Rank.six
+        },
+        right: {
+          suit: Suit.diamond,
+          rank: Rank.seven
+        }
+      },
+      actions: [
+        {
+          playerIndex: 0,
+          betSize: 1.0
+        },
+        {
+          playerIndex: 1,
+          betSize: 0
+        }
+      ]
+    }
+  } as unknown) as GameSituation,
+  createdAt: new Date(),
+  lastUpdatedAt: new Date()
+};
---
+++ components/PostCardList/PostCardList.tsx
@@ -0,0 +1,34 @@
+import * as React from "react";
+import styled from "styled-components";
+import { MOBILE_MEDIA } from "../../constants/mediaquery";
+import ShowLastUpdateDateContext from "./ShowLastUpdateDateContext";
+
+interface Props extends React.Attributes {
+  showLastUpdateDate?: boolean;
+  className?: string;
+  style?: React.CSSProperties;
+  children?: React.ReactNode;
+}
+
+export default function PostCardList({
+  showLastUpdateDate = false,
+  children
+}: Props) {
+  return (
+    <Root>
+      <ShowLastUpdateDateContext.Provider value={showLastUpdateDate}>
+        {children}
+      </ShowLastUpdateDateContext.Provider>
+    </Root>
+  );
+}
+
+const Root = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(350px, 1fr));
+  grid-gap: 16px;
+
+  ${MOBILE_MEDIA} {
+    grid-template-columns: 100%;
+  }
+`;
