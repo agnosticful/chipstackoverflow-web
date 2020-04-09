19ea470a8a8f3694b6fb6e9dae02b86d04350432
Kohei Asai <yo@kohei.dev>
2020-04-08 18:40:09 -0700

    Setup FullStory

--- next.config.js
+++ next.config.js
@@ -8,7 +8,8 @@ module.exports = {
       storageBucket: "chipstackoverflow.appspot.com",
       messagingSenderId: "373238358080",
       appId: "1:373238358080:web:ab932ac823e7dca9e63fa5",
-      measurementId: "G-7081C73TLP"
-    })
-  }
+      measurementId: "G-7081C73TLP",
+    }),
+    FULLSTORY_ORGANIZATION_ID: "T7V6F",
+  },
 };
--- repositories/anonymizeUserForLogging.ts
+++ repositories/anonymizeUserForLogging.ts
@@ -4,10 +4,14 @@ export type AnonymizeUserForLogging = () => void;
 
 export function createAnonymizeUserForLogging({
   firebaseApp,
+  fullstory,
 }: {
   firebaseApp: firebase.app.App;
+  fullstory: any;
 }): AnonymizeUserForLogging {
   return () => {
     firebaseApp.analytics().setUserId(null as any, { global: true });
+
+    fullstory?.anonymize();
   };
 }
