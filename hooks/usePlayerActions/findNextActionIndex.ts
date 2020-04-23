699d0c74e8053481c0e54d01519995f2645970c3
ryotogashi <ryo.togashi.ca@gmail.com>
2020-04-22 19:30:03 -0700

    Add findNextActionIndex

---
+++ hooks/usePlayerActions/findNextActionIndex.ts
@@ -0,0 +1,17 @@
+import { GameStreetAction } from "../../models/GameSituation";
+
+export default function findNextActionIndex({
+  playerIndex,
+  actions,
+  start,
+}: {
+  playerIndex: number;
+  actions: GameStreetAction[];
+  start: number;
+}) {
+  const nextActionIndex = actions
+    .slice(start)
+    .findIndex((action) => action.playerIndex === playerIndex);
+
+  return nextActionIndex === -1 ? -1 : nextActionIndex + start;
+}
