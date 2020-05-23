7a318bdb80166308441138e1dd12a7861d632d8e
ryotogashi <ryo.togashi.ca@gmail.com>
2020-05-23 12:24:28 -0700

    new post page

---
+++ hooks/usePlayerActions/createEmptyGameStreetActions.ts
@@ -0,0 +1,18 @@
+import { GameStreetActionType } from "../../models/GameSituation";
+import { Street } from "./playerActionReducer";
+
+export default function createEmptyGameStreetActions({
+  street,
+  playerLength,
+}: {
+  street: Street;
+  playerLength: number;
+}) {
+  return street === Street.preflop
+    ? Array.from({ length: playerLength }, (_, playerIndex) => ({
+        type: GameStreetActionType.fold,
+        playerIndex,
+        betSize: 0,
+      }))
+    : [];
+}
---
+++ hooks/usePlayerActions/playerActionReducer.ts
@@ -0,0 +1,6 @@
+export enum Street {
+  preflop = "PREFLOP",
+  flop = "FLOP",
+  turn = "TURN",
+  river = "RIVER",
+}
