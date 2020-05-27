c9c660e72cd8ec2c5770b8ea0fd41fb16d4d50f6
Kohei Asai <yo@kohei.dev>
2020-05-27 09:58:57 -0700

    Implement <HandPlayerContentLoader> (#265)

---
+++ components/HandPlayerContentLoader/HandPlayerContentLoader.stories.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+import HandPlayerContentLoader from "@@/components/HandPlayerContentLoader";
+
+export default {
+  title: "HandPlayerContentLoader",
+  component: HandPlayerContentLoader,
+};
+
+export const example = () => <HandPlayerContentLoader />;
