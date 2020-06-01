790aadc9fc8009c3614aed394c1e9eb6ac4938cd
ryotogashi <ryo.togashi.ca@gmail.com>
2020-05-31 22:34:45 -0700

    Add GameType to GameSituation

--- pageComponents/NewPostPage/GameTypeSelect.tsx
+++ pageComponents/NewPostPage/GameTypeSelect.tsx
@@ -1,6 +1,5 @@
 import * as React from "react";
 import Select, { Option } from "../../components/Select";
-import { GameType } from "../../models/GameSituation";
 
 interface Props extends React.Attributes {
   defaultValue?: string;
@@ -20,4 +19,9 @@ export default function GameTypeSelect({
       <Option value={GameType.tournament}>{GameType.tournament}</Option>
     </Select>
   );
+}
+
+enum GameType {
+  cash = "CASH",
+  tournament = "TOURNAMENT",
 }
