f43f6b297e53fbc35d99e7848cf13bec968f8d83
Kohei Asai <yo@kohei.dev>
2020-06-02 21:10:46 -0700

    Install Bugsnag (#281)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,10 @@
+import Bugsnag from "@bugsnag/js";
+
+Bugsnag.start({
+  apiKey: process.env.NEXT_PUBLIC_BUGSNAG_API_KEY!,
+  releaseStage: process.env.NEXT_PUBLIC_BUGSNAG_RELEASE_STAGE,
+});
+
 import { ApolloProvider } from "@apollo/react-hooks";
 import { ApolloClient, HttpLink, InMemoryCache } from "apollo-boost";
 import { AppProps } from "next/app";
@@ -19,28 +26,45 @@ function App(props: AppProps) {
   return <props.Component {...props.pageProps} />;
 }
 
-export default (props: AppProps) => {
-  const apolloClient = React.useMemo(
-    () =>
-      new ApolloClient({
-        link: new HttpLink({ uri: process.env.NEXT_PUBLIC_API_ENDPOINT }),
-        cache: new InMemoryCache(),
-        defaultOptions: {
-          query: { fetchPolicy: "no-cache" },
-          mutate: { fetchPolicy: "no-cache" },
-        },
-      }),
-    [process.env.NEXT_PUBLIC_API_ENDPOINT]
-  );
+export default class extends React.Component<AppProps> {
+  private apolloClient = new ApolloClient({
+    link: new HttpLink({ uri: process.env.NEXT_PUBLIC_API_ENDPOINT }),
+    cache: new InMemoryCache(),
+    defaultOptions: {
+      query: { fetchPolicy: "no-cache" },
+      mutate: { fetchPolicy: "no-cache" },
+    },
+  });
 
-  return (
-    <RecoilRoot>
-      <ApolloProvider client={apolloClient}>
-        <App {...props} />
-      </ApolloProvider>
-    </RecoilRoot>
-  );
-};
+  componentDidCatch(error: Error, info: React.ErrorInfo) {
+    Bugsnag.notify(error, (event) => {
+      const reactMetadata = {
+        ...info,
+        componentStack: info.componentStack
+          .split(/\s*\n\s*/g)
+          .reduce((formatted, line) => {
+            if (line.length === 0) {
+              return formatted;
+            }
+
+            return `${formatted}\n${line}`;
+          }, ""),
+      };
+
+      event.addMetadata("react", reactMetadata);
+    });
+  }
+
+  render() {
+    return (
+      <RecoilRoot>
+        <ApolloProvider client={this.apolloClient}>
+          <App {...this.props} />
+        </ApolloProvider>
+      </RecoilRoot>
+    );
+  }
+}
 
 export function reportWebVitals(metric: any) {
   console.info(metric);
